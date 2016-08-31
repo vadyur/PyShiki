992228c60044b140edc86e33c0a735364d8c0cdf
OlegWock <olegwock@gmail.com>
2016-08-31 11:12:29 +0300

    v 1.1.5

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ from distutils.core import setup
 
 setup(
     name='pyshiki',
-    version='1.1.4',
+    version='1.1.5',
     author='OlegWock',
     author_email='olegwock@gmail.com',
     url='https://github.com/OlegWock/PyShiki',
